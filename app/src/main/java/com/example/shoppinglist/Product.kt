d29fea800a2794b244f08932dfbed63d603d9072
my vu <myxvux@gmail.com>
2019-10-12 17:12:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/shoppinglist/Product.kt
@@ -0,0 +1,22 @@
+package com.example.shoppinglist
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "product_table")
+data class Product(
+
+    @ColumnInfo( name = "name")
+    var name: String,
+
+    @ColumnInfo ( name = "quantity")
+    var quantity: Int,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo( name = "id")
+    var id: Long? = null
+) : Parcelable
